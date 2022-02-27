9ace20a81584436b92d58cdf8e173738eaf2b5f8
marcus <marcmonk@gmail.com>
2022-02-26 21:11:36 -0500

    starting ch 5 - routing

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,19 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
 import { QuestionList } from './QuestionsList';
-import { getUnansweredQuestions } from './QuestionsData';
+import { getUnansweredQuestions, QuestionData } from './QuestionsData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
+import { PrimaryButton } from './Styles';
 
 export const HomePage = () => {
+  const [questions, setQuestions] = React.useState<QuestionData[]>([]);
+  const [questionsLoading, setQuestionsLoading] = React.useState(true);
+  React.useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+
+  const handleAskQuestionClick = () => {
+    console.log('TODO - move to the AskPage');
+  };
+
   return (
     <Page>
-      <div>
-        <div>
-          <PageTitle>Unanswered Questions</PageTitle>
-          <button>Ask a question</button>
-        </div>
-        {/* <QuestionList data={getUnansweredQuestions()} /> */}
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
+        <PageTitle>Unanswered Questions</PageTitle>
+        <PrimaryButton onClick={handleAskQuestionClick}>
+          Ask a question
+        </PrimaryButton>
       </div>
+      {questionsLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <QuestionList data={questions || []} />
+      )}
     </Page>
   );
 };
---
+++ src/QuestionPage.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { Page } from './Page';
+export const QuestionPage = () => <Page>Question Page</Page>;
