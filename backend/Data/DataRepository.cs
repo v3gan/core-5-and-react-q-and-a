8f00fa0793f64ecbbe997f2be34e67efaf1516ab
marcus <marcmonk@gmail.com>
2022-02-28 22:04:22 -0500

    reading data using dapper

---
+++ backend/Data/DataRepository.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using QandA.Data.Models;
+
+namespace QandA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+        public DataRepository(IConfiguration configuraion)
+        {
+            _connectionString = configuraion["ConnectionStrings:DefaultConnection"];
+        }
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirstOrDefault<AnswerGetResponse>(
+                    @"EXEC dbo.Answer_Get_ByAnswerId @AnswerId = @AnswerId",
+                    new { AnswerId = answerId }
+                );
+            }
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var question =
+                connection.QueryFirstOrDefault<QuestionGetSingleResponse>(
+                    @"EXEC dbo.Question_GetSingle @QuestionId = @QuestionId",
+                    new { 
+                        QuestionId = questionId 
+                    }
+                );
+                if (question != null)
+                {
+                    question.Answers = 
+                        connection.Query<AnswerGetResponse>(
+                                @"EXEC dbo.Answer_Get_ByQuestionId @QuestionId = @QuestionId",
+                                new
+                                {
+                                    QuestionId = questionId
+                                }
+                            );
+                    return question; 
+                }
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dob.Question_GetMany");
+            };
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    @"EXEC dbo.Question_GetMany_BySearch @Search = @Search", new { 
+                        Search = search 
+                    });
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    "EXEC dbo.Question_GetUnanswered"
+                );
+            }
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.QueryFirst<bool>(
+                    @"EXEC dbo.Question_Exists @QuestionId = @QuestionId", 
+                    new { QuestionId = questionId }
+                );
+            }
+        }
+    }
+}
